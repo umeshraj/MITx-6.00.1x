b6289f50c5c39b9764c69bf6dd54eadb35e087b2
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-02-09 15:07:13 -0500

    Code to play a full game

--- week4/ProblemSet4/ps4a.py
+++ week4/ProblemSet4/ps4a.py
@@ -295,9 +295,23 @@ def playGame(wordList):
     2) When done playing the hand, repeat from step 1
     """
     # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
+    #print("playGame not yet implemented.") # <-- Remove this line when you code the function
+    hand = {}
 
-
+    while True:
+        user_choice = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if user_choice == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, n=HAND_SIZE)
+        elif user_choice == 'r':
+            if hand == {}:  # no previous hand
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                playHand(hand, wordList, n=HAND_SIZE)
+        elif user_choice == 'e':
+            break
+        else:
+            print('Invalid command.')
 
 
 #
