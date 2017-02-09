efbb4f25404357d925bba90ddd34289d2490e6ac
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-02-09 14:28:54 -0500

    Calculate length of current hand

--- week4/ProblemSet4/ps4a.py
+++ week4/ProblemSet4/ps4a.py
@@ -200,6 +200,10 @@ def calculateHandlen(hand):
     returns: integer
     """
     # TO DO... <-- Remove this comment when you code this function
+    hand_len = 0
+    for val in hand.values():
+        hand_len += val
+    return hand_len
 
 
 
