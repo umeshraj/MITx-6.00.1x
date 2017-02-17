22dc3e2c3099c37a1b7586585790c075fbae41e8
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-02-17 12:31:43 -0500

    Implementing methods for encrypting a plain message

--- week5/ps6/ps6.py
+++ week5/ps6/ps6.py
@@ -156,6 +156,10 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less
         code is repeated
         '''
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
         pass #delete this line and replace with your code here
 
     def get_shift(self):
@@ -164,7 +168,8 @@ class PlaintextMessage(Message):
 
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        #pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
@@ -172,7 +177,9 @@ class PlaintextMessage(Message):
 
         Returns: a COPY of self.encrypting_dict
         '''
-        pass #delete this line and replace with your code here
+        return self.encrypting_dict.copy()
+        #pass #delete this line and replace with your code here
+
 
     def get_message_text_encrypted(self):
         '''
@@ -180,7 +187,8 @@ class PlaintextMessage(Message):
 
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        #pass #delete this line and replace with your code here
+        return self.message_text_encrypted[:]
 
     def change_shift(self, shift):
         '''
@@ -193,7 +201,11 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
+        #pass #delete this line and replace with your code here
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
+
 
 
 class CiphertextMessage(Message):
